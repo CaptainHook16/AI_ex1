d52cea9304de9e3537a1b911b1156112746987aa
CaptainHook16 <45101692+CaptainHook16@users.noreply.github.com>
2018-11-24 21:38:52 +0200

    Add files via upload

---
+++ ex1_2311/src/AlgoInterface.java
@@ -0,0 +1,15 @@
+
+
+//collection of abstract methods used in our Algorithms.
+public interface AlgoInterface {
+
+    public int numberOutOfOpenList();
+
+    public String findBestPath();
+
+    //bfs - 0, A* - path cost, IDS - depth
+    public int thirdValueToPrint();
+
+    public void StartAlgorithm(BoardGame firstBoard);
+
+}
---
+++ ex1_2311/src/Enum.java
@@ -0,0 +1,19 @@
+
+
+public class Enum {
+
+    enum Algorithm{
+        BFS,
+        ASTAR,
+        IDS
+    }
+
+    enum Direction{
+        Right,
+        Left,
+        Down,
+        Up
+    }
+
+
+}
